38bbfac9296af22b51340ba9f105900c4fc376d3
dayraivana <130511804+dayraivana@users.noreply.github.com>
2024-11-26 16:46:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -273,11 +273,12 @@ def main():
         print(f"Error procesando el evento: {e}")
 
 if __name__ == "__main__":
+    main()
     # Simula un evento configurando manualmente la variable de entorno
-    os.environ["CE_SUBSCRIBED_EVENT"] = json.dumps({
-        "bucket_name": "poc-asbanc",   # Nombre de tu bucket
-        "object_key": "1. Carta BCP ok.pdf"  # Nombre de tu archivo PDF
-    })
+   # os.environ["CE_SUBSCRIBED_EVENT"] = json.dumps({
+    #    "bucket_name": "poc-asbanc",   # Nombre de tu bucket
+    #    "object_key": "1. Carta BCP ok.pdf"  # Nombre de tu archivo PDF
+   # })
 
     # Ejecutar la función principal
-    main()
+    
